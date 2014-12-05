9b264b80cd9505c938fb3eae2ab7a1078e7f17f5
Marek Skrobacki <skrobul@skrobul.com>
2014-12-05 15:55:15 +0000

    add working version of notification

---
+++ lib/clearmsg_handler.rb
@@ -0,0 +1,40 @@
+require 'celluloid'
+require_relative './aggregated_message.rb'
+class ClearMessageHandler
+  include Celluloid
+
+  def initialize(notifier, clearing_interval)
+    @notifier = notifier
+    @msgs = []
+    @clearing_interval = clearing_interval
+  end
+
+  def handle(msg)
+    @msgs << msg
+  end
+
+  def handle_messages
+    sleep @clearing_interval
+    send_all_messages_in_the_buffer
+  end
+
+  def terminate
+    send_all_messages_in_the_buffer
+  end
+
+  private
+
+  def clear_message_queue
+    @msgs = []
+  end
+
+  def send_all_messages_in_the_buffer
+    if @msgs.size > 0
+      aggregated_messsage = AggregatedMessage.new(@msgs, :clear)
+      clear_message_queue
+      @notifier.notify aggregated_messsage
+    end
+    async.handle_messages
+  end
+
+end
--- lib/notification_pipe.rb
+++ lib/notification_pipe.rb
@@ -1,16 +1,19 @@
 require_relative "./time_providers"
 require_relative './aggregated_message'
+require_relative './clearmsg_handler'
+require 'celluloid/autostart'
+
 
 class NotificationPipe
+  include Celluloid
+
   def initialize(opts={})
     @notifier = opts.fetch(:notifier)
     @deluge_threshold = opts.fetch(:deluge_threshold, 60)
     @clearing_interval = opts.fetch(:clearing_interval, 60)
-    @clear_buffer = []
     @timesrc = opts.fetch(:timesrc, NormalTimeProvider.new)
-    @clear_buffer_last_flushed = @timesrc.now
     @last_problem_notification = nil
-    start_clearing_thread
+    start_clearmsg_handler
   end
 
   def process(msg)
@@ -19,22 +22,16 @@ class NotificationPipe
       when :problem
         send_deluge_notification(msg) if deluge_timer_expired?
       when :clear
-        if clear_timer_expired?
-          empty_clear_buffer
-        else
-          add_clearmsg_to_buffer msg
-        end
+        @clear_msg_handler.async.handle msg
     end
   end
 
   private
-    def start_clearing_thread
-      # TODO: convert me to celluloid !
-      @t = Thread.new do
-        @timesrc.sleep(@clearing_interval)
-        empty_clear_buffer
-      end
+    def start_clearmsg_handler
+      @clear_msg_handler = ClearMessageHandler.new(@notifier, @clearing_interval)
+      @clear_msg_handler.async.handle_messages
     end
+
     def send_deluge_notification(msg)
       @last_problem_notification = @timesrc.now
       @notifier.notify msg
@@ -42,18 +39,5 @@ class NotificationPipe
 
     def deluge_timer_expired?
       @last_problem_notification.nil? || @timesrc.now >= (@last_problem_notification + @deluge_threshold)
-    end
-
-    def clear_timer_expired?
-      @timesrc.now >= (@clear_buffer_last_flushed + @clearing_interval)
-    end
-
-    def empty_clear_buffer
-      aggregated_messsage = AggregatedMessage.new(@clear_buffer)
-      @notifier.notify aggregated_messsage
-    end
-
-    def add_clearmsg_to_buffer(msg)
-      @clear_buffer << msg
     end
 end
